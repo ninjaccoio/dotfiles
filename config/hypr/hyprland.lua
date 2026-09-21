2c7a664ca895470452a3188eb650decc1b9fb252
ninjaccoio <saviniloris@gmail.com>
2026-09-21 14:04:11 +0200

    configure display and touchpad

--- config/hypr/hyprland.lua
+++ config/hypr/hyprland.lua
@@ -244,7 +244,9 @@ hl.config({
         sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
 
         touchpad = {
-            natural_scroll = false,
+            natural_scroll = true,
+            tap_to_click = true,
+            disable_while_typing = true,
         },
     },
 })
